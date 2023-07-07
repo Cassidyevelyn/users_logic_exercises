0cc59b638445e2f135bd7c5ff14fc90d7a9c16a8
Cassidy Armstrong <carmstrong1@gmail.com>
2023-07-07 14:10:12 +0100

    user question 5 complete

--- users_exercise.py
+++ users_exercise.py
@@ -70,6 +70,11 @@ print(users["Avril"]["pets"][0]["species"])
 
 
 # 5. Get the smallest of Erik's lottery numbers
+smallest_number = min(users["Erik"]["lottery_numbers"])
+print(smallest_number)
+
+
+
 # 6. Return an list of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
